083215b9abeca25c2923ef3ac9a9492d6ffa236c
Yamamoto <maki.mk1929.mt6927@gmail.com>
2016-05-20 18:15:02 +0900

    20160520

--- 02/contact.php
+++ 02/contact.php
@@ -1,94 +1,142 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<!--CSS-->
-<link rel="stylesheet" type="text/css" href="default.css">
-<title>お問い合わせ</title>
-<meta charset="utf-8">
-    <body>
+    <!--CSS-->
+    <link rel="stylesheet" type="text/css" href="default.css">
+    <title>お問い合わせ</title>
+    <meta charset="utf-8">
+
+    <body  style="font-family:'メイリオ',Meiryo;">
         <!--全体の枠-->
         <div id="warp">
-        <!--見出し-->
-        <h1>お問い合わせフォーム</h1>
+            <!--見出し-->
+            <h1>お問い合わせフォーム</h1>
 
-        <!--formのサイズ-->
-        <div id="main">
-            <div id="main1">
-                <!--フォーム-->
-                <form id="form" method="post" action="result.php">
+            <!--formのサイズ-->
+            <div id="main">
+                <div id="main1">
+                    <!--フォーム-->
+                    <form id="form" method="post" action="result.php">
 
-                <!--苗字-->
-                <div id="name_left1">姓</div>
-                <div id="name_right1"><input type="text" name="name"></div>
-                <div class="clear_box"></div>
+                        <div class="main_bar">
+                        <!--苗字-->
+                        <div id="name_left1">姓</div>
+                        <div id="name_right1">
+                            <input type="text" name="name">
+                        </div>
+                    </div>
+                        <div class="clear_box"></div>
 
-                <!--名前-->
-                <div id="name_left2">名</div>
-                <div id="name_right3"><input type="text" name="name2"></div>
-                <div class="clear_box"></div>
+                        <div class="main_bar">
+                        <!--名前-->
+                        <div id="name_left1">名</div>
+                        <div id="name_right1">
+                            <input type="text" name="name2">
+                        </div>
+                        </div>
+                        <div class="clear_box"></div>
 
+                        <div class="main_bar">
+                        <!--性別-->
+                        <!--idを追加して文字をクリックしてもチェックがつくようにするためlabel追加-->
+                        <div id="name_left1">性別</div>
+                        <div id="name_right1">
+                            <input type="radio" name="sex" id="m" 　value="男性">
+                            <label for="m">男性</label>
+                            <input type="radio" name="sex" id="w" 　value="女性">
+                            <label for="w">女性</label>
+                            <input type="radio" name="sex" id="z" 　value="不明">
+                            <label for="z">不明</label>
+                        </div>
+                    </div>
+                        <div class="clear_box"></div>
 
-                <!--性別-->
-                <!--idを追加して文字をクリックしてもチェックがつくようにするためlabel追加-->
-                性別
-                <input type="radio" name="sex"　value="1"><label for="m">男性
-                <input type="radio" name="sex"　value="2"><label for="w">女性
-                <input type="radio" name="sex"　value="3"><label for="z">不明
-                <div class="clear_box"></div>
+                        <div class="main_bar">
+                        <!--住所-->
+                        <div id="name_left1">住所</div>
+                        <div id="name_right1">
+                            <input type="textbox" name="address" size="60">
+                        </div>
+                    </div>
+                        <div class="clear_box"></div>
 
-                <!--住所-->
-                住所
-                <input type="textarea" name="address" size="60">
-                <div class="clear_box"></div>
+                        <div class="main_bar">
+                        <!--電話番号-->
+                        <div id="name_left1">電話番号</div>
+                        <div id="name_right1">
+                            <div class="overflow">
+                                <input type="text" name="number"> -
+                                <input type="text" name="number2"> -
+                                <input type="text" name="number3">
+                            </div>
+                        </div>
+                        </div>
+                        <div class="clear_box"></div>
 
-                <!--電話番号-->
-                電話番号
-                    <input type="text" name="number"> - <input type="text" name="number2"> - <input type="text" name="number3">
-                <div class="clear_box"></div>
+                        <div class="main_bar">
+                        <!--メールアドレス-->
+                        <div id="name_left1">メールアドレス</div>
+                        <div id="name_right1">
+                            <input type="text" name="email"> @
+                            <input type="text" name="email2">
+                        </div>
+                    </div>
+                        <div class="clear_box"></div>
 
+                        <div class="main_bar">
+                        <!--どこでサイトを知りましたか-->
+                        <div id="name_left1">どこで知りましたか</div>
+                        <div id="name_right1">
+                            <input type="checkbox" name="abc" id="a" value="1">
+                            <label for="a">インターネット</label>
+                            <input type="checkbox" name="abc" id="b" value="2">
+                            <label for="b">広告･新聞</label>
+                            <input type="checkbox" name="abc" id="c" value="3">
+                            <label for="c">家族・親戚</label>
+                        </div>
+                    </div>
+                        <div class="clear_box"></div>
 
-                <!--メールアドレス-->
-                メールアドレス
-                    <input type="text" name="email"> @ <input type="text" name="email2">
-                <div class="clear_box"></div>
+                        <div class="main_bar">
+                        <!--質問カテゴリ-->
+                        <div id="name_left1">質問カテゴリ</div>
+                        <div id="name_right1">
+                            <select name="question">
+                                <option value="">--選択してください--</option>
+                                <option value="1">不明点</option>
+                                <option value="2">故障</option>
+                                <option value="3">その他</option>
+                            </select>
+                        </div>
+                    </div>
+                        <div class="clear_box"></div>
 
-
-                <!--どこでサイトを知りましたか-->
-                どこでサイトを知りましたか
-                    <input type="checkbox" name="abc" value="1"><label for="1">インターネット
-                    <input type="checkbox" name="abc" value="2"><label for="2">広告
-                    <input type="checkbox" name="abc" value="3"><label for="3">家族・親戚
-                <div class="clear_box"></div>
-
-                <!--質問カテゴリ-->
-                質問カテゴリ
-                <select name="question">
-                    <option value="">--選択してください--</option>
-                    <option value="1">感想</option>
-                    <option value="2">故障</option>
-                    <option value="3">不明点</option>
-                </select>
-                <div class="clear_box"></div>
-            </div>
+                </div>
 
                 <div id="main2">
-                <!--質問内容-->
-                質問内容
-                    <textarea name="question1" cols="40" rows="5" maxlength="30" placeholder="質問内容をご記入ください"></textarea>
-                <div class="clear_box"></div>
+                    <!--質問内容-->
+                    <div id="name_left1">質問内容</div>
+                    <!--<div class="clear_box"></div>-->
+                    <div id="textarea">
+                        <textarea name="question1" cols="70" rows="5" maxlength="50" placeholder="質問内容をご記入ください"></textarea>
+                    </div>
+                    <div class="clear_box"></div>
+                </div>
 
-                <!--送信ボタン-->
-                <input type="submit" value="送信">
+                <div id="main3">
+                    <div id="button">
+                        <!--送信ボタン-->
+                        <input type="submit" id="submit_button" value="送信">
 
-                <!--リセットボタン-->
-                <input type="reset" value="リセット"><br />
-                <div class="clear_box"></div>
-            </div>
+                        <!--リセットボタン-->
+                        <input type="reset" id="reset_button" value="リセット">
+                        <br />
+                        <div class="clear_box"></div>
+                    </div>
+                </div>
 
-
-        </form>
-
-        </div>
+                </form>
 
         </div>
 
@@ -96,4 +144,5 @@
     </body>
 
 </head>
+
 </html>
--- 02/result.php
+++ 02/result.php
@@ -16,14 +16,120 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email2 = $_POST["email2"];
     $abc = $_POST["abc"];
 }
+
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-    <body>
-        <?php
-        echo $question;
-        ?>
+    <!--CSS-->
+    <link rel="stylesheet" type="text/css" href="default.css">
+    <title>お問い合わせ</title>
+    <meta charset="utf-8">
+
+    <body  style="font-family:'メイリオ',Meiryo;">
+        <!--全体の枠-->
+        <div id="warp">
+            <!--見出し-->
+            <h1>お問い合わせフォーム</h1>
+
+            <!--formのサイズ-->
+            <div id="main">
+                <div id="main1">
+                    <!--フォーム-->
+                    <form id="form" method="post" action="contact.php">
+
+                        <div class="main_bar">
+                        <!--苗字-->
+                        <div id="name_left1">姓名</div>
+                        <div id="name_right1">
+                            <?php echo $name.'&nbsp;&nbsp;'.$name2; ?>
+                        </div>
+                    </div>
+                        <div class="clear_box"></div>
+
+                        <div class="main_bar">
+                        <!--性別-->
+                        <!--idを追加して文字をクリックしてもチェックがつくようにするためlabel追加-->
+                        <div id="name_left1">性別</div>
+                        <div id="name_right1">
+                            <?php echo $sex; ?>
+                        </div>
+                    </div>
+                        <div class="clear_box"></div>
+
+                        <div class="main_bar">
+                        <!--住所-->
+                        <div id="name_left1">住所</div>
+                        <div id="name_right1">
+                            <?php echo $address; ?>
+                        </div>
+                    </div>
+                        <div class="clear_box"></div>
+
+                        <div class="main_bar">
+                        <!--電話番号-->
+                        <div id="name_left1">電話番号</div>
+                        <div id="name_right1">
+                            <?php echo $number."-".$number2."-".$number3; ?>
+                        </div>
+                        </div>
+                        <div class="clear_box"></div>
+
+                        <div class="main_bar">
+                        <!--メールアドレス-->
+                        <div id="name_left1">メールアドレス</div>
+                        <div id="name_right1">
+                            <?php echo $email."@".$email2 ?>
+                        </div>
+                    </div>
+                        <div class="clear_box"></div>
+
+                        <div class="main_bar">
+                        <!--どこでサイトを知りましたか-->
+                        <div id="name_left1">どこで知りましたか</div>
+                        <div id="name_right1">
+                            <?php echo $abc; ?>
+                        </div>
+                    </div>
+                        <div class="clear_box"></div>
+
+                        <div class="main_bar">
+                        <!--質問カテゴリ-->
+                        <div id="name_left1">質問カテゴリ</div>
+                        <div id="name_right1">
+                            <?php echo $question; ?>
+                        </div>
+                    </div>
+                        <div class="clear_box"></div>
+
+                </div>
+
+                <div id="main2">
+                    <!--質問内容-->
+                    <div id="name_left1">質問内容</div>
+                    <!--<div class="clear_box"></div>-->
+                    <div id="name_right1">
+                        <?php echo $question1; ?>
+                    </div>
+                    <div class="clear_box"></div>
+                </div>
+
+                <div id="main3">
+                    <div id="button">
+                        <!--送信ボタン-->
+                        <input type="submit" id="submit_button" value="送信">
+                        <div class="clear_box"></div>
+                    </div>
+                </div>
+
+                </form>
+
+        </div>
+
+
     </body>
+
 </head>
+
 </html>
